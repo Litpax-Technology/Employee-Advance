895483e28aa944e0c1bda09f8093d3ffe8b5aab6
mis-Darpan <mis@litpaxtechnology.com>
2026-07-01 15:21:31 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 // =============================================
 
 const CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbxP2bkPzJkSvYY0uN9DHHpmYCGDlRH0NUjZ381TXjTPlbPfbEnkKigxXBt2rObhJjxO/exec",  // ← Sirf yahan URL daalo
+  GAS_URL: "https://script.google.com/macros/s/AKfycbx76cW082FnZ7Q0iX3oYamPw_I5uS_OwcT7fXjK8PCXfQQfR7lO7YtnDhH3GyBPk5_Q/exec",  // ← Sirf yahan URL daalo
   COMPANY_NAME: "Litpax Technology Pvt. Ltd.",
   SYSTEM_NAME: "Employee Advance System",
 };
